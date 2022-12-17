eeb4018a19c36ff9fd82d3c6cbad065f49f4f1b8
Oliver Chen <hchen7@wpi.edu>
2022-12-17 00:06:07 -0500

    first commit

---
+++ assets/js/LightBox.js
@@ -0,0 +1,35 @@
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
+
+const images = document.querySelectorAll('img')
+images.forEach(image => {
+  image.addEventListener('click', e => {
+    lightbox.classList.add('active')
+    const img = document.createElement('img')
+    img.src = image.src
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild)
+    }
+    lightbox.appendChild(img)
+  })
+  image.addEventListener('touchend', e => {
+    lightbox.classList.add('active')
+    const img = document.createElement('img')
+    img.src = image.src
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild)
+    }
+    lightbox.appendChild(img)
+  })
+})
+
+lightbox.addEventListener('click', e => {
+  if (e.target !== e.currentTarget) return
+  lightbox.classList.remove('active')
+})
+
+lightbox.addEventListener('touchend', e => {
+    if (e.target !== e.currentTarget) return
+    lightbox.classList.remove('active')
+  })
